be8cf11b65d337f08e30826b8c6d3aac0f65e040
Anton Zuev <anzuev@bk.ru>
2016-03-30 10:29:30 +0300

    Build 30-03-2016
    1) Added auth API description for mainServer
    2) Added universities and faculties API description for staticData
    3) Added API description for staticServer(uploading files and download)

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 'use strict';
 
-var app = require('connect')();
+var connect = require('connect');
 var http = require('http');
-var swaggerTools = require('swagger-tools');
-var jsyaml = require('js-yaml');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 var fs = require('fs');
+var serveStatic = require('serve-static');
+var path = require('path');
+
+
+
 var serverPort = 3005;
+var app = connect();
 
-// swaggerRouter configuration
-var options = {
-  swaggerUi: '/swagger.json',
-  controllers: './controllers',
-  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
-};
 
-// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync('./api/swagger.yaml', 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
-
-// Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
-
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
-
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
-
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
-
-  // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-  });
-});
+app
+	.use(logger('dev'))
+	.use(serveStatic('public'))
+	.listen(serverPort, function(err, result){
+		if(err) throw err;
+		else{
+			console.log("Сервер запущен, слушаем %d порт", serverPort);
+		}
+	});
